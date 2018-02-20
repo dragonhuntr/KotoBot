01e3ff14888979f22f5e336f2a72944124052176
dragonhuntr <31814492+dragonhuntr@users.noreply.github.com>
2018-02-20 23:38:51 +0800

    Update tip.js

--- src/commands/tip.js
+++ src/commands/tip.js
@@ -1,5 +1,12 @@
 const { OOPS_TEXT } = require('../messages')
 
+const WITHDRAW_TEXT = 'Successful withdrawal.'
+const PROPER_AMOUNT_TEXT = 'You need to provide the amount of Koto to send.'
+const NO_COMMA_TEXT = 'Please use "." instead of ",".'
+const NEED_ADDRESS_TEXT = 'Please specify a Koto t-address as your third argument.'
+const NO_FUNDS = 'You do not have any Koto in your wallet, please add Koto using the address provided in **-koto address**.'
+const NOT_ENOUGH_FUNDS = 'You do not have enough Koto to send, please do **-koto address** to check your balance.'
+
 var multilevel = require('multilevel')
 var levelup = require('levelup')
 var leveldown = require('leveldown')
@@ -16,9 +23,9 @@ var Zcash = require("zcash");
 
 const http = require('http');
 
-const rpcLogin = ‘insert-rpc-username-here’;
-const rpcPassword = ‘insert-rpc-password-here’;
-const rpcHost = ‘insert-rpc-ip-here’;
+const rpcLogin = 'kotosan';
+const rpcPassword = 'tekitou';
+const rpcHost = '127.0.0.1';
 const rpcPort = 8432;
 
 const txFee = 0.0001;
@@ -55,20 +62,21 @@ function post(method, params, callback) {
  
 // Execute RPC z_sendmany
 function tip (message, Zcash, amount, toAccount) {
-  var amountInt = parseInt(amount)
+  var amountInt = parseFloat(amount).toFixed(8);
+  console.log(amountInt)
   if (!amountInt) {
     message.reply(PROPER_AMOUNT_TEXT)
     return
   }
 
-  // If amount has a comma
-  if (amount.indexOf(',') >= 0) {
-    message.reply(NO_COMMA_TEXT)
-    return
+  // If amount has a hashtag
+  if (toAccount.indexOf('#') >= 0) {
+    toAccount = toAccount.replace('#', '');
+    console.log(toAccount)
   }
 
-  if (!toAddress) {
-    message.reply(NEED_ADDRESS_TEXT)
+  if (!toAccount) {
+    message.reply(message.author.toString() + ' ,' + NEED_ADDRESS_TEXT)
     return
   }
 
@@ -84,35 +92,42 @@ function tip (message, Zcash, amount, toAccount) {
         }
   db.get(toAccount, function(err, address) {
     var haddress = addr + address
+    if (haddress  == '' + 'undefined') {
+      message.channel.send(message.author.toString() + ', User specified not found.')
+      return;
+    }
     console.log(haddress)
-    post('z_getbalance', [haddress], function(err, data) {
-        if (err != null) {
-            console.log('Error: ' + err.message);
-            message.channel.send('User does not have address.')
-            return;
-        }
-        
-        if (data.result >= (threshold - txFee)) {
-            var amount = data.result - txFee;
+      if (data.result >= amountInt) {
+        if (data.result >= amountInt - threshold - txFee) {
+          console.log(data.result >= amountInt - threshold - txFee)
+          var amount = data.result - txFee;
+            console.log(amountInt)
             var bal = amount - amountInt;
-            console.log('Transfer', amountInt, 'from', gaddress, 'to', toAddress);
-            post('z_sendmany', [gaddress, [{ address: toAddress, amount: amountInt }, { address: gaddress, amount: bal }]], function(err, data) {
+            bal = bal.toFixed(8)
+            console.log('Transfer', amountInt, 'from', gaddress, 'to', haddress);
+            post('z_sendmany', [gaddress, [{ address: haddress, amount: amountInt }, { address: gaddress, amount: bal }]], function(err, data) {
                 if (err != null) {
                     console.log('Error: ' + e.message);
                     message.channel.send(message.author.toString() + ' ERROR, please contact Limit#1844')
                 } else {
                     console.log(data)
-                    console.log('Sent', amountInt, 'to', toAddress);
-                    message.channel.send(message.author.toString() + ', Sent **' +  amountInt + '** Koto to ``' +  toAddress + '``')
+                    console.log('Sent', amountInt, 'to', haddress);
+                    message.channel.send(message.author.toString() + ', Sent **' +  amountInt + '** Koto to ``' +  haddress + '``')
                     message.channel.send('Your balance should update once the transaction gets confirmed.')
                 }
             });
         } else {
             console.log('Balance', data.result, 'is below threshold');
-            message.channel.send('You do not have enough balance');
+            message.channel.send(message.author.toString() + 'Your tipping amount is below the threshold of **0.1** Koto. Please raise the amount.');
+              }
+            }
+        else { 
+           console.log ('Not enough balance')
+           message.channel.send(message.author.toString() + ', You do not have enough balance/ Your tipping amount is below the threshold of **0.1** Koto. Please raise the amount.');
         }
-    });
-  })
+        });
+      })
+    })
 }
- 
+  
 module.exports = tip
